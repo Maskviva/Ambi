a9947dab826cf8bde3e48414f0ca1ff6074a75dc
Maskviva <maskviva@qq.com>
2026-04-20 19:39:53 +0800

    加入texts目录测试agent流水线

---
+++ tests/api_test.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod tests {
+    use ambi::{Agent, EngineConfig, OpenAIEngineConfig};
+
+    #[tokio::test]
+    async fn test_ollama_local_api() {
+        let base_url =
+            std::env::var("TEST_BASE_URL").unwrap_or("https://api.openai.com/v1".to_string());
+        let model_name = std::env::var("TEST_MODEL_NAME").unwrap_or("gpt-4o-mini".to_string());
+
+        let cfg = OpenAIEngineConfig {
+            api_key: "test".to_string(),
+            base_url,
+            model_name,
+            temp: 0.7,
+            top_p: 0.9,
+        };
+
+        let mut agent = Agent::make(EngineConfig::OpenAI(cfg)).unwrap();
+
+        let res = agent.chat("who are you").await;
+
+        println!("{}", res);
+    }
+}
